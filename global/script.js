9490ef20c4a5ef561a76541ab123397b2982ec22
Diogo Medeiros <85233292+diogohmedeiros@users.noreply.github.com>
2022-11-14 09:20:49 -0300

    Add files via upload

---
+++ global/script.js
@@ -0,0 +1,13 @@
+var logohome = document.getElementById('logo-home').addEventListener("click", () => {
+  window.location.href = "../home";
+})
+
+var footerwhatsapp = document.getElementById('footer-whatsapp').addEventListener("click", () => {
+  window.location.href = "https://wa.me/5519999881273";
+})
+var footerfacebook = document.getElementById('footer-facebook').addEventListener("click", () => {
+  window.location.href = "https://web.facebook.com/informaticajaguariuna/";
+})
+var footerinstagram = document.getElementById('footer-instagram').addEventListener("click", () => {
+  window.location.href = "https://www.instagram.com/newlaninformatica/";
+})
